9a6beaa65d00741473906e2af548130f9f7e02f3
@Junaid5415 <ajunaid.edu@gmail.com>
2024-01-28 12:15:16 +0530

    commit

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,5 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #  Manually Added
 STATICFILES_DIRS = [
-    BASE_DIR, 'static'
+    os.path.join(BASE_DIR, '/static')
+
 ]
