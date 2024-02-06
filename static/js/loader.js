1128a391c379c4132d77d7a1dcafba70bb64712a
@Junaid5415 <ajunaid.edu@gmail.com>
2024-02-06 19:27:29 +0530

    New Commit

---
+++ static/js/loader.js
@@ -0,0 +1,17 @@
+// const adiv = document.querySelector('div');
+// const body = document.querySelector('#myBody');
+
+// const span = adiv.querySelector('span');
+
+// function myfunc(){
+//     body.onload = adiv.style.display = 'none';
+//     body.onload = span.style.scale = '100';
+//     // body.style.display = 'none';
+// }
+
+// function loaded(){
+//     console.log('started');
+//     setTimeout(function(){
+//         myfunc()
+//     },1900);
+// }
